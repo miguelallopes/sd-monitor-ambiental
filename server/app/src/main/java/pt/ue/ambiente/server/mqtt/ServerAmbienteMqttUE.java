a8aa2b2a6864c9990c613df74b628fbaad3f0cd8
Miguel Lopes <miguel.lopes@miguelallopes.dev>
2025-12-28 20:58:33 +0000

    refactor: mqtt and rest
    Co-authored-by: MiguelPinto20 <MiguelPinto20@users.noreply.github.com>
    Signed-off-by: Miguel Lopes <miguel.lopes@miguelallopes.dev>

--- server/app/src/main/java/pt/ue/ambiente/server/mqtt/ServerAmbienteMqttUE.java
+++ server/app/src/main/java/pt/ue/ambiente/server/mqtt/ServerAmbienteMqttUE.java
@@ -1,5 +1,14 @@
 package pt.ue.ambiente.server.mqtt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import pt.ue.ambiente.server.data.ServerAmbienteDataUE;
+
 public class ServerAmbienteMqttUE {
-    
+    @Autowired
+    private final ServerAmbienteDataUE repositories;
+
+    public ServerAmbienteMqttUE(ServerAmbienteDataUE repositories) {
+        this.repositories = repositories;
+    }
 }
--- server/app/src/main/java/pt/ue/ambiente/server/rest/ServerAmbienteRestUE.java
+++ server/app/src/main/java/pt/ue/ambiente/server/rest/ServerAmbienteRestUE.java
@@ -1,5 +1,17 @@
 package pt.ue.ambiente.server.rest;
 
+import org.springframework.stereotype.Controller;
+
+import pt.ue.ambiente.server.data.ServerAmbienteDataUE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
 public class ServerAmbienteRestUE {
-    
+    @Autowired
+    private final ServerAmbienteDataUE repositories;
+
+    public ServerAmbienteRestUE(ServerAmbienteDataUE repositories) {
+        this.repositories = repositories;
+    }
 }
